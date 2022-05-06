43dd1e50cf1aeefdf2f358b6ca1336fed316c079
Breno Sampaio <br.sampaio@outlook.com>
2022-05-05 22:53:07 -0300

    Ok
    Calculadora básica finalizada.

--- Iniciante/Calculadora/calculadora.py
+++ Iniciante/Calculadora/calculadora.py
@@ -6,24 +6,29 @@ print("2 - subtração")
 print("3 - multiplicação")
 print("4 - divisão\n")
 
-operação = int(input("Qual operação deseja?\n"))
+while True:
+    operação = int(input("Qual operação deseja?\n"))
+    if operação > 0 and operação < 5:
+        break
+    else:
+        print("Você não digitou uma operação válida!\n")
+
 
 i_num_1 = int(input("Digite o primeiro número:"))
 i_num_2 = int(input("Digite o segundo número:"))
 
 if (operação == 1):
     soma = i_num_1 + i_num_2
-    print("{} + {} = {}".format(i_num_1, i_num_2, soma))
+    print("\n {} + {} = {}".format(i_num_1, i_num_2, soma))
 elif (operação == 2):
     subtracao = i_num_1 - i_num_2
-    print("{} - {} = {}".format(i_num_1, i_num_2, subtracao))
+    print("\n {} - {} = {}".format(i_num_1, i_num_2, subtracao))
 elif (operação == 3):
     multiplicacao = i_num_1 * i_num_2
-    print("{} * {} = {}".format(i_num_1, i_num_2, multiplicacao))
+    print("\n {} * {} = {}".format(i_num_1, i_num_2, multiplicacao))
 elif (operação == 4):
     divisao = i_num_1 / i_num_2
-    print("{} / {} = {}".format(i_num_1, i_num_2, divisao))
-else:
-    print("Você não digitou uma operação válida!")
+    print("\n {} / {} = {}".format(i_num_1, i_num_2, divisao))
+
 
 
