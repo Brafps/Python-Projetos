76fedff16729a62fd66bd46ecb54b833c9926276
Breno Sampaio <br.sampaio@outlook.com>
2022-05-17 16:54:26 -0300

    Update calculadora.py

--- Iniciante/Calculadora/calculadora.py
+++ Iniciante/Calculadora/calculadora.py
@@ -1,50 +1,132 @@
+'''
+Depósito
+else:
+    print("\033[0;31m Erro! Digite um valor de operação válido.\033[m")
+print("---------------------------------------------------\nA Soma dos números digitados é igual a: %f\n" %sum(l_lista))    
+'''
 # Definição de operações
 
 def soma():
-    lista = []
-    contador = 1
+    print("Digite os números que deseja efetuar a soma:")
+    f_soma = 0.0
+    i_contador = 0
     while True:    
-            f_num_flash = input(f"Digite o #{contador} número ou S para sair desse menu:")
-            if f_num_flash == ("s" or "S"):
-                break
-            elif f_num_flash.isnumeric():
-                lista.append(float(f_num_flash))
-                contador += 1
+        s_num_flash = input()
+        if s_num_flash == ("s" or "S"):
+            break
+        elif type(float(s_num_flash)) == type(0.0):
+            f_soma += float(s_num_flash)
+            i_contador += 1
+            if i_contador>=2:
+                print("Digite S a qualquer momento para ter o resultado da soma ou digite um novo valor.")
+    print("-----------------------------------------------")
+    return f_soma
 
-            else:
-                print("\033[0;31m Erro! Digite um valor de operação válido.\033[m")
-    return print("A Soma dos números digitados é igual a: %f\n" %sum(lista))
+def diferença():
+    f_minuendo = float(input("Digite o valor do minuendo:"))
+    f_subtraendo = float(input("Digite o valor do subtraendo:"))
+    print("-----------------------------------------------")
+    return f_minuendo - f_subtraendo
 
-# Tela FIxa
+def produto():
+    print("Digite os números que deseja efetuar o produto:")
+    f_produto = 1.0
+    i_contador = 0
+    while True:    
+        s_num_flash = input()
+        if s_num_flash == ("s" or "S"):
+            break
+        elif type(float(s_num_flash)) == type(0.0):
+            f_produto = f_produto*float(s_num_flash)
+            i_contador += 1
+            if i_contador>=2:
+                print("Digite S a qualquer momento para ter o resultado da soma ou digite um novo valor.")
+    print("-----------------------------------------------")
+    return f_produto
 
-print("~~~~~~ Calculadora em Python ~~~~~~\n")
 
-print("Abaixo, digite o valor da operação correspondente. ")
+def divisao():
+    f_dividendo = float(input("Digite o valor do dividendo:"))
+    f_divisor = float(input("Digite o valor não nulo para o divisor:"))
+    print("-----------------------------------------------")
+    return f_dividendo/f_divisor
+
+def f_m_aritmetica():
+    print("Digite os números que deseja efetuar a média:")
+    f_soma = 0.0
+    i_contador = 0
+    while True:    
+        s_num_flash = input()
+        if s_num_flash == ("s" or "S"):
+            break
+        elif type(float(s_num_flash)) == type(0.0):
+            f_soma += float(s_num_flash)
+            i_contador += 1
+            if i_contador>=2:
+                print("Digite S a qualquer momento para ter o resultado ou digite um novo valor.")
+    print("-----------------------------------------------")
+    return f_soma/i_contador
 
 while True:
+# Tela FIxa
+
+    print('''~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~ Calculadora em Python ~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n''')
+
+    print("Abaixo, digite o valor da operação correspondente.\n")
+
+#menus
+
     print('''[1] - Soma
 [2] - Subtração
 [3] - Multiplicação
 [4] - Divisão
 [5] - Média Aritmética
 [6] - Média Ponderada
-[0] - Encerrar''')
+[0] - Encerrar\n''')
 
     while True:
-        operação = int(input("\nQual operação deseja?\n"))
-        if operação >= 0 and operação <= 9:
+        i_operação = int(input("Qual operação deseja?\n"))
+        if i_operação >= 0 and i_operação <= 9:
             break
         else:
             print("\033[0;31m Erro! Digite um valor de operação válido.\033[m")
 # Condições
 
-    if (operação == 1):
-        soma()
+    if (i_operação == 1):
+        print("-----------------------------------------------")
+        print("A Soma dos números digitados é: %f" %soma())
+        print("-----------------------------------------------\n")
+
+    if (i_operação == 2):
+        print("-----------------------------------------------")
+        print("A diferença dos números digitados é: %f" %diferença())
+        print("-----------------------------------------------\n")        
+    
+    if (i_operação == 3):
+        print("-----------------------------------------------")
+        print("O produto dos números digitados é: %f" %produto())
+        print("-----------------------------------------------\n")
+
+    if (i_operação == 4):
+        print("-----------------------------------------------")
+        print("O quociente dos números digitados é: %f" %divisao())
+        print("-----------------------------------------------\n")
+
+    if (i_operação == 5):
+        print("-----------------------------------------------")
+        print("A média aritmética dos números digitados é: %f" %f_m_aritmetica())
+        print("-----------------------------------------------\n")
+
+    if (i_operação == 6):
+        print("-----------------------------------------------")
+        print("Falta definir essa operação")
+        print("-----------------------------------------------\n")
 
 # Condição para finalizar o programa 
-        
-    
-    elif (operação == 0):
+   
+    elif (i_operação == 0):
         print("Finalizando...")
         break
  
